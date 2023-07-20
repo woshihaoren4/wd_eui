224d3dac72d678efdf598ce9c94f764c95aea955
wangdong.teshin <wangdong.teshin@bytedance.com>
2023-07-20 20:45:18 +0800

    设置icon

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::sync::Arc;
 use eframe::egui::vec2;
+use eframe::IconData;
 use crate::infra::HttpPool;
 
 mod app;
 mod infra;
+
+const ICON:&'static [u8] = include_bytes!("../logo.png");
 
 fn main() -> eframe::Result<()> {
 
@@ -13,6 +16,7 @@ fn main() -> eframe::Result<()> {
 
     let opt = eframe::NativeOptions{
         initial_window_size : Some(vec2(400.0,300.0)),
+        icon_data:Some(IconData::try_from_png_bytes(ICON).unwrap()),
         ..Default::default()
     };
     eframe::run_native("♻️this is a test app ♻️",opt,Box::new(|x|{
